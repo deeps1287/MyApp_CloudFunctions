8b31a3352b16c8c676f399a44efc46ec5c7f50ff
Deepak Shinde <deepakshinde1287@gmail.com>
2021-10-13 11:49:13 +0530

    changes

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const https = require("http");
 
 exports.helloWorld = functions.https.onRequest((req, res) => {
-  res.send("Hello from Firebase to Deepak !");
+  res.send("Hello from Firebase to Deepak / staging !");
 });
 
 exports.productSearch = functions.https.onRequest((request, response) => {
